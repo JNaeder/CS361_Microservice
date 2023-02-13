3aa5aa141c6d86da8ba9b26cdb540ca51e314819
JNaeder <J.naeder324@gmail.com>
2023-02-13 11:43:44 -0500

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+// Import stuff
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const phrases = require("./phrases");
+
+// Variables
+const app = express();
+const port = 5000;
+const weatherAPI = process.env.WEATHER_KEY;
+
+// My Functions
+const getLocationData = async function (lat, long) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${lat},${long}&aqi=no`;
+  const output = await axios.get(url);
+  return output["data"];
+};
+
+const getRandomPhrase = function (phraseArray) {
+  const randomNumber = Math.trunc(Math.random() * phraseArray.length);
+  return phraseArray[randomNumber];
+};
+
+const getTempPhrase = function (tempF) {
+  if (tempF < 50) {
+    return getRandomPhrase(phrases.coldPhrases);
+  } else if (tempF > 90) {
+    return getRandomPhrase(phrases.hotPhrases);
+  } else {
+    return getRandomPhrase(phrases.perfectPhrases);
+  }
+};
+const makeOutputData = function (locationData) {
+  const tempF = locationData["current"]["temp_f"];
+  const output = {
+    location_name: `${locationData["location"]["name"]}, ${locationData["location"]["region"]}`,
+    country: `${locationData["location"]["country"]}`,
+    temp_celsius: `${locationData["current"]["temp_c"]}`,
+    temp_fahrenheit: `${tempF}`,
+    phrase: getTempPhrase(tempF),
+  };
+  return output;
+};
+
+// Express Funtions
+app.get(
+  "/weather",
+  asyncHandler(async (req, res) => {
+    const locationData = await getLocationData(req.query.lat, req.query.long);
+    const output = makeOutputData(locationData);
+    res.send(output);
+  })
+);
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ phrases.js
@@ -0,0 +1,20 @@
+const coldPhrases = [
+  "Feels like I'm riding on ice instead of concrete.",
+  "I'm freezing my tail off out here.",
+  "Can't feel my toes anymore.",
+  "This weather is harsh for a skate sesh.",
+  "I'm getting frostbite one trick at a time.",
+  "It's so cold, my board is sticking to the ground.",
+  "This wind chill is no joke.",
+  "I need to hit a café and warm up after this.",
+  "Skating in this weather is a challenge, but I'm up for it.",
+  "I might as well be snowboarding, it's so cold out here.",
+];
+
+const hotPhrases = ["It's pretty boiling yo"];
+
+const perfectPhrases = ["It can't get better than this"];
+
+exports.coldPhrases = coldPhrases;
+exports.hotPhrases = hotPhrases;
+exports.perfectPhrases = perfectPhrases;
