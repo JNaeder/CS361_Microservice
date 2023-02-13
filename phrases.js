d7bbce3ce5360541c87754762181cbda0acc6ed8
JNaeder <J.naeder324@gmail.com>
2023-02-13 12:34:20 -0500

    Added new phrases

--- phrases.js
+++ phrases.js
@@ -11,9 +11,31 @@ const coldPhrases = [
   "I might as well be snowboarding, it's so cold out here.",
 ];
 
-const hotPhrases = ["It's pretty boiling yo"];
+const hotPhrases = [
+  "This heat is sapping all my energy.",
+  "Skating in this heat is no picnic.",
+  "I feel like I'm melting out here.",
+  "I need a break, it's too hot to keep going.",
+  "This concrete is hot enough to fry an egg.",
+  "I need a cold drink, stat.",
+  "It's a good day to be a pool skater.",
+  "Why is it always so hot when I want to skate?",
+  "I feel like I'm in a sauna on wheels.",
+  "This heat is making it hard to grip my board.",
+];
 
-const perfectPhrases = ["It can't get better than this"];
+const perfectPhrases = [
+  "This weather is a skater's paradise.",
+  "Perfect conditions for a session.",
+  "The sun is shining, the concrete is calling.",
+  "Can't ask for better weather to skate in.",
+  "This is why I love skating, days like this.",
+  "The wind is just right, let's go.",
+  "Skating in this weather is a dream come true.",
+  "I could stay out here all day in this weather.",
+  "It's the ideal temperature to shred all day.",
+  "Every skater should have weather like this to skate in.",
+];
 
 exports.coldPhrases = coldPhrases;
 exports.hotPhrases = hotPhrases;
